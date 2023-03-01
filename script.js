64fb5ad224af8f4698c010681fdf01b01b012fe7
n377i <annettewoehler@gmx.de>
2023-03-01 01:23:22 +0100

    add options

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 "use strict";
 
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", ":", 
-                    ";", "<", ">", ".", "?", "/"];
+let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", ":", ";", "<", ">", ".", "?", "/"];
 
 const passwordEl = document.getElementById("password");
 const lengthEl = document.getElementById("length");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
 const generateBtn = document.getElementById("generate");
 const copyBtn = document.getElementById("copy");
   
 const generatePassword = () => {
   passwordEl.textContent = "";
   let length = lengthEl.value;
+  characters.length = 52;
+  if (numbersEl.checked) {
+    characters = characters.concat(numbers);
+  }
+  if (symbolsEl.checked) {
+    characters = characters.concat(symbols);
+  }
   for (let i = 0; i < length; i++) {
     let randomNumber = Math.floor(Math.random() * characters.length);
     let randomCharacter = characters[randomNumber];
     passwordEl.textContent += randomCharacter;
   }
-};
+}
 generateBtn.addEventListener("click", generatePassword);
 
 const copyPassword = () => {
   navigator.clipboard.writeText(passwordEl.textContent);
-};
+}
 copyBtn.addEventListener("click", copyPassword);
